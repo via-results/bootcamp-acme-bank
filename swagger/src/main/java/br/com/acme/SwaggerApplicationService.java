e97faed8f967ef953db7a00f44c9c957f1f3df7f
cbarbosagomesfilho <cbarbosagomesfilho@gmail.com>
2025-08-17 11:36:51 -0300

    feat: clean project,create gitignore files to all project

---
+++ swagger/src/main/java/br/com/acme/SwaggerApplicationService.java
@@ -0,0 +1,11 @@
+package br.com.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplicationService {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplicationService.class, args);
+    }
+}
